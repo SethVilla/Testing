42f41c139334f4ebf66f891c744de065d63f958c
Seth Villa <sethvilla13@gmail.com>
2021-03-15 23:05:22 -0400

    Babel config prevent prod data attributes

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App" data-test="component-app">
-      <h1>App</h1>
+      <h1 data-test="counter-display"> the counter is 1</h1>
+      <button data-test="increment-button">Increment Button</button>
     </div>
   );
 }
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -5,27 +5,37 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 //set up Enzyme Adapter
 Enzyme.configure({adapter: new Adapter()});
 
+/**
+ * Factory function to create a Shallow Wrapper for the app component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App/>);
+
+// save from remembering Enzyme data type
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 test("renders without error", 
 ()=>{
   //data tests attribute to target top level component
   // data tests removed during production
   const wrapper = shallow(<App/>);
-
   //find 
   //https://enzymejs.github.io/enzyme/docs/api/ShallowWrapper/find.html
-    const appComponent = wrapper.find("[data-test='component-app']")
-
+    const appComponent = findByTestAttr(wrapper, 'component-app')
     expect(appComponent.length).toBe(1);
-
-
 });
 
 test("renders button", ()=>{
-
+  const wrapper = shallow(<App/>);
+  const incrementButton = wrapper.find("[data-test='increment-button']")
+  expect(incrementButton.length).toBe(1);
 });
 
 test("renders counter display", ()=>{
-
+  const wrapper = shallow(<App/>);
+  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  expect(counterDisplay.length).toBe(1);
 });
 
 test("counters starts 0", ()=>{
