3edd2a92a4394052f76e6d1ee686d206da30fccb
Seth Villa <sethvilla13@gmail.com>
2021-03-17 21:27:48 -0400

    testing practice

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -10,6 +10,33 @@ const GuessedWords = (props) => {
                 Try to guess the secret word!
             </span>
         )
+    } else {
+        const guessedWordRows = props.guessedWords.map((word, index) => 
+            <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        )
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                               Guess
+                            </th>
+                            <th>
+                                Matching Letter
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {guessedWordRows}
+                    </tbody>
+                </table>
+            </div>
+        )
     }
     return (
         <div data-test="component-guessed-words">
--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -40,7 +40,36 @@ describe("Guesswords test", () => {
     })
 
     describe("if there are words guessed", () => {
-        
+        let wrapper;
+        const guessedWords = [
+            {
+                guessedWord: 'train', letterMatchCount: 3
+            },
+            {
+                guessedWord: 'agile', letterMatchCount: 1
+            },
+            {
+                guessedWord: 'party', letterMatchCount: 5
+            }
+        ];
+        beforeEach(() => {
+            wrapper = setup({guessedWords})
+        })
+        it("renders without error", () => {
+            const component = findByTestAttr(wrapper, 'component-guessed-words');
+            expect(component.length).toBe(1)
+        })
+
+        it("renders guessed words section", () => {
+            const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words')
+            expect(guessedWordsNode.length).toBe(1)
+        })
+
+        it('correct number of guessed words', () => {
+            const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word')
+            expect(guessedWordNodes.length).toBe(guessedWords.length)
+        })
+
     })
 })
 
